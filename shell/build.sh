30202c16ded2dd1b72267d64d966366253ccb432
Joel Davis <jodavis@redhat.com>
2019-07-18 09:03:34 -0400

    renaming GCP playbooks

--- shell/build.sh
+++ shell/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ansible-playbook ansible/${targetEnvironment}-provision.yml > ansible-provision-${targetEnvironment}.log
-ansible-playbook -i /tmp/inventory ansible/${targetEnvironment}-setup.yml > ansible-setup-${targetEnvironment}.log
-ansible-playbook -i /tmp/inventory ansible/${targetEnvironment}-mkimage.yml > ansible-mkimage-${targetEnvironment}.log
+ansible-playbook --private-key ${SSH_KEY_LOCATION} -i /tmp/inventory ansible/${targetEnvironment}-setup.yml > ansible-setup-${targetEnvironment}.log
+ansible-playbook --private-key ${SSH_KEY_LOCATION} -i /tmp/inventory ansible/${targetEnvironment}-mkimage.yml > ansible-mkimage-${targetEnvironment}.log
