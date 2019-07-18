37e41e24a9151081871ded11f43782a57b304d82
Joel Davis <jodavis@redhat.com>
2019-07-18 09:25:54 -0400

    adding publish script

--- shell/publish.sh
+++ shell/publish.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-echo "Placeholder for publish images script"
+#################
+## Publish images to user accessible portions of their respective cloud providers. 
+##
+## Minikube is intentionally omitted since that's not intended for end user consumption and is just used for running tests.
+## No real functional reason this file is separate from build.sh outside of letting Jenkins show this as being its own stage
+## since it takes so long (lets build initiators tell progress is being made).
+#################
+
+set -x
+
+ansible-playbook ansible/${targetEnvironment}-provision.yml > ansible-publish-${targetEnvironment}.log 2>&1
